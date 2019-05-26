792452db53f38536383d8420e74b318c2fdbe1f6
Yodsawadee Sornda <jan.yodsawadee@outlook.com>
2019-05-26 19:08:13 +0700

    add password

---
+++ iqa_web/confidential_password.py
@@ -0,0 +1,29 @@
+import settings
+
+key = 'qj=kov0a8j7o@#o2bm119&_o8_fn3&5san!h+^ixk%r!29gwhy'
+
+###databse localhost
+##name = 'iqa3'
+##user = 'postgres'
+##password = '1234'
+##host = 'localhost'
+##port = 5432
+
+###databse remote sever
+name = 'df6o8lha7r1edj'
+user = 'fxpxjtxfhrvnco'
+password = '3cd7c82b1f942a3c225d71a5bade351c59f90460bddaef56704a48849cae737c'
+host = 'ec2-46-137-113-157.eu-west-1.compute.amazonaws.com'
+port = 5432
+URI = 'postgres://fxpxjtxfhrvnco:3cd7c82b1f942a3c225d71a5bade351c59f90460bddaef56704a48849cae737c@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/df6o8lha7r1edj'
+Heroku CLI = 'heroku pg:psql postgresql-angular-66522 --app iqa-kmitl'
+
+
+##'ENGINE': 'django.db.backends.postgresql_psycopg2',
+##'NAME': 'd1n23cgnd7ja1p',
+##'USER': 'geibdlrcdjqrco',
+##'PASSWORD': '4ec973bde7ae11d0418d99d15db606f783ee63b7d12eb02e2ae520e45a1f2908',
+##'HOST': 'ec2-54-221-253-228.compute-1.amazonaws.com',
+##'PORT': '5432',
+
+
